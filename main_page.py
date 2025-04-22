1c733e0ad1c9190b0ff82542914a473567e55a06
mizzzzord <vlasovamasha2207@mail.ru>
2025-04-22 23:26:50 +0500

    Delete main_page.py

--- main_page.py
+++
@@ -1,7 +0,0 @@
-from .base_page import BasePage
-from .login_page import LoginPage
-from .locators import MainPageLocators
-
-class MainPage(BasePage): 
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
