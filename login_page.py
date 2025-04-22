9f9b067556c10dfefd8e4e5d0cf2d9f6a8642d61
mizzzzord <vlasovamasha2207@mail.ru>
2025-04-22 23:06:42 +0500

    Create login_page.py

---
+++ login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def register_new_user(self,email, password):
+        self.browser.find_element(*LoginPageLocators.REG_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REG_PASS1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REG_PASS2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REG_BUTTON).click()
+    
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.url, "Url without 'login'"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+       
